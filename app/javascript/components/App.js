0bfad6d7530223d26e747a46752d508509dbf8ba
lcnde <176673026+lcnde@users.noreply.github.com>
2021-12-21 20:40:15 +0100

    connect react frontend

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Router from '../routes/Router'
+import Home from '../components/Home'
+
+
 
 const App = () => {
   return(
-    <h1>Hello React</h1>
+    <Router />
   ) 
 }
 
---
+++ app/javascript/components/Home.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  return(
+    <h1>Home page</h1>
+  ) 
+}
+
+export default Home;
