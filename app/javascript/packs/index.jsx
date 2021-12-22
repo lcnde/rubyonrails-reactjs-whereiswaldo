e4339e819c6a2ad562303a9683d8118a6c321452
lcnde <176673026+lcnde@users.noreply.github.com>
2021-12-22 05:59:12 +0100

    make compatibility with turbolinks

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../components/App'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <App />,
     document.body.appendChild(document.createElement('div')),
