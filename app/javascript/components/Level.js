fd9d6a0ac90dd6a7c91cec13d1c204425f628b9a
lcnde <176673026+lcnde@users.noreply.github.com>
2021-12-25 18:28:29 +0100

    fetch and display data from database

--- app/javascript/components/Level.js
+++ app/javascript/components/Level.js
@@ -3,8 +3,45 @@ import { Link } from 'react-router-dom';
 
 
 const Level = () => {
+  const [coords, setCoords] = useState([])
+
+  //equals to componentDidMount
+  useEffect(() => {
+    const url = "/api/v1/show/1"
+    fetch(url).then(response => {
+      if (response.status === 200){
+        console.log('Status 200')
+        return response.json();
+      } else {
+        throw new Error('Something went wrong')
+      }
+    }).then(data => {
+        console.log('Request successful')
+        console.log('Request data: ', data)
+        data.map((element) => {
+          // console.log(element)
+          setCoords(prevState => [...prevState, element])
+        })
+      }).catch(err => console.log(err))
+    }, [])
+    
+    console.log("Current coords status:", coords)
+
   return(
-    <h1>Level 1 page</h1>
+    <React.Fragment>
+      <h1>Level 1 page</h1>
+      <Link
+        to="/"
+      >
+        Go back to home
+      </Link>
+      <br/>
+      {
+        coords.map(data => 
+          <h1>{data.x_coords}</h1>
+        )
+      }
+    </React.Fragment>
   ) 
 }
 
