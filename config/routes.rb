85f21a8c5b19d7405b6f789174b4e27c73f5f005
lcnde <176673026+lcnde@users.noreply.github.com>
2022-01-01 10:53:33 +0100

    feature: added routes for new levels controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'coords/index'
-      get '/show/:map', to: 'coords#show'
+      # get 'coords/index'
+      # get '/show/:map', to: 'coords#show'
+      get 'levels/index'
+      get 'levels/show/:map_id', to: 'levels#show'
     end
   end
 
